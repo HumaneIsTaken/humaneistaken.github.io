918d7e8c33ba9beaab76d8612fa86d62ad3c6e95
Humane <33730758+HumaneIsTaken@users.noreply.github.com>
2019-01-31 01:34:34 +0100

    Added scroll transition to touch based devices.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,5 @@
 var landingScrolled = false;
+var animLocked = false;
 
 const landingContainerElem = 
     document.getElementById("landing-container");
@@ -6,7 +7,7 @@ const infoContainerElem =
     document.getElementById("info-page-container");
 
 var scrollFromLanding = (event) => {
-    if (event.deltaY > 0 && !landingScrolled) {
+    if (event.deltaY > 0 && !landingScrolled && !animLocked) {
         landingSrolled = true;
         landingContainerElem.classList.add("hidden");
         infoContainerElem.classList.remove("hidden");
@@ -16,9 +17,11 @@ var scrollFromLanding = (event) => {
 // Add scroll event listener/functions for mobile devices.
 
 window.addEventListener("wheel", scrollFromLanding);
+window.addEventListener("scroll", scrollFromLanding);
 
 
 var onLoadLandingAnims = () => {
+    animLocked = true;
     const name = document.getElementById("name-big");
     const info = document.getElementById("info-big");
     const jobTitle = document.getElementById("jtitle-wrapper");
@@ -54,10 +57,48 @@ var onLoadLandingAnims = () => {
     setTimeout(() => {
         jobTitle.children[0].style.height = "auto";
         jobTitle.children[0].style.opacity = "1";
-        
+        animLocked = false;
     }, 3500);
 
 
 }
 
 window.onload = onLoadLandingAnims();
+
+
+// credit: http://www.javascriptkit.com/javatutors/touchevents2.shtml
+// and https://codepen.io/ganmahmud/pen/RaoKZa?editors=0010
+// Modified version of the script that only cares about the Y axis and
+// threshold of the scroll.
+function swipeDetect(el, callback){
+  
+    var touchsurface = el,
+    swipedir,
+    startY,
+    distY,
+    threshold = 100, //required min distance traveled to be considered swipe
+    handleswipe = callback || function(swipedir){}
+  
+    touchsurface.addEventListener('touchstart', function(e){
+        var touchobj = e.changedTouches[0]
+        swipedir = 'none'
+        startY = touchobj.pageY
+    }, false)
+  
+    touchsurface.addEventListener('touchmove', function(e){
+        var touchobj = e.changedTouches[0]
+        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+        if (Math.abs(distY) >= threshold ){ // 2nd condition for vertical swipe met
+            swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+        }
+        handleswipe(swipedir)
+    }, false)
+}
+
+swipeDetect(window, (dir) => {
+    if (!landingScrolled && !animLocked && dir == "up") {
+        landingSrolled = true;
+        landingContainerElem.classList.add("hidden");
+        infoContainerElem.classList.remove("hidden");
+    }
+});
